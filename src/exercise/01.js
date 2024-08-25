63d2aa94a29585878457b61054ecf980df44d7a6
mukhammadjondev <muhsdev@gmail.com>
2024-08-25 10:34:10 +0500

    create resource promise

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,23 +2,40 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
+import {
+  PokemonDataView,
+  fetchPokemon,
+  PokemonErrorBoundary,
+  PokemonInfoFallback,
+} from '../pokemon'
+import {createResource} from 'utils'
 
-let pokemon
-let pokemonError
+// function createResource(promise) {
+//   let status = 'pending'
+//   let result = promise.then(
+//     resolved => {
+//       status = 'success'
+//       result = resolved
+//     },
+//     rejected => {
+//       status = 'error'
+//       result = rejected
+//     },
+//   )
+//   return {
+//     read() {
+//       if (status === 'pending') throw result
+//       if (status === 'error') throw result
+//       if (status === 'success') return result
+//       throw new Error('This should be impossible')
+//     },
+//   }
+// }
 
-const pokemonPromise = fetchPokemon('pikachu').then(
-  result => (pokemon = result),
-  error => (pokemonError = error),
-)
+let pokemonResource = createResource(fetchPokemon('pikachu'))
 
 function PokemonInfo() {
-  if (pokemonError) {
-    throw pokemonError
-  }
-  if (!pokemon) {
-    throw pokemonPromise
-  }
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -34,7 +51,7 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <React.Suspense fallback={<PokemonInfoFallback name="Pikachu" />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>
